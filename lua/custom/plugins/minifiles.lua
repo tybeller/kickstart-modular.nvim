ad24831c9688e9ee6394a186b1e237fb37ce26db
tybeller <ty.c.beller@gmail.com>
2024-10-07 00:05:55 -0400

    added friendly snippets and changed minifiles keybind

--- lua/custom/plugins/minifiles.lua
+++ lua/custom/plugins/minifiles.lua
@@ -2,9 +2,7 @@ return {
   'echasnovski/mini.files',
   config = function()
     require('mini.files').setup {
-      mappings = {
-        close = '<ESC>',
-      },
+      mappings = {},
     }
     vim.keymap.set('n', '<leader>e', ':lua MiniFiles.open()<CR>', { desc = '[E]xplore Filetree' })
   end,
