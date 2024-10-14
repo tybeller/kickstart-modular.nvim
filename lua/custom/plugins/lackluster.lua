1e64ad6fb5751df7607011eacf606e07793c9c58
tybeller <ty.c.beller@gmail.com>
2024-10-14 01:22:49 -0400

    changed theme to lackluster

---
+++ lua/custom/plugins/lackluster.lua
@@ -0,0 +1,19 @@
+return {
+  'slugbyte/lackluster.nvim',
+  lazy = false,
+  priority = 1000,
+  init = function()
+    local lackluster = require 'lackluster'
+    -- !must called setup() before setting the colorscheme!
+    require('nvim-web-devicons').setup {
+      color_icons = false,
+      override = {
+        ['default_icon'] = {
+          color = lackluster.color.gray4,
+          name = 'Default',
+        },
+      },
+    }
+    vim.cmd.colorscheme 'lackluster-hack' -- my favorite
+  end,
+}
--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -63,9 +63,9 @@ return {
         -- },
         -- pickers = {}
         extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
+          -- ['ui-select'] = {
+          --    require('telescope.themes').get_dropdown(),
+          -- },
         },
       }
 
