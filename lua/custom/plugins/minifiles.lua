52d048419ee0e0942cfda383e3416f81fe5affb2
tybeller <ty.c.beller@gmail.com>
2024-11-09 22:48:02 -0500

    SUPERCOLLIDER

--- lua/custom/plugins/minifiles.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'echasnovski/mini.files',
-  config = function()
-    require('mini.files').setup {
-      mappings = {},
-    }
-    vim.keymap.set('n', '<leader>e', ':lua MiniFiles.open()<CR>', { desc = '[E]xplore Filetree' })
-  end,
-}
