9bb1d63be933c861dcc6a620aed125f8c061b07c
tybeller <ty.c.beller@gmail.com>
2024-12-29 16:04:50 -0500

    removed lackluster, changed theme and added transparency support.

--- lua/custom/plugins/tinted.lua
+++ lua/custom/plugins/tinted.lua
@@ -3,6 +3,7 @@ return {
   priority = 1000,
   config = function()
     vim.g.tinted_enable = 1
-    vim.cmd.colorscheme 'base16-black-metal'
+    vim.g.tinted_background_transparent = 1
+    vim.cmd.colorscheme 'base16-gruvbox-material-dark-hard'
   end,
 }
