4f19d6da0af66eb1a4a73bb2b4799ad1e83f40ec
tybeller <ty.c.beller@gmail.com>
2024-12-29 20:30:48 -0500

    switched to rose pine i guess for now sigh

--- lua/custom/plugins/tinted.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'tinted-theming/tinted-vim',
-  priority = 1000,
-  config = function()
-    vim.g.tinted_enable = 1
-    vim.g.tinted_background_transparent = 1
-    vim.cmd.colorscheme 'base16-gruvbox-material-dark-hard'
-  end,
-}
