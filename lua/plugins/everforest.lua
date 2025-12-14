9b72a1a792123c6d7e5820234aae40d234a4c98f
Ty Beller <ty.c.beller@gmail.com>
2025-12-14 12:44:24 -0500

    whatever

--- lua/plugins/everforest.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'sainnhe/everforest',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.g.everforest_enable_italic = true
-        vim.cmd.colorscheme 'everforest'
-    end,
-}
